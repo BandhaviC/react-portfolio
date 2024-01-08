e87b0155e7e03d66c3fcdd2419ad8ec4829c404a
BandhaviC <bandhavic6@gmail.com>
2024-01-08 17:26:14 -0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-portfolio/', // Adjust this based on your repository name
   plugins: [react()],
   css:{
     modules: {
