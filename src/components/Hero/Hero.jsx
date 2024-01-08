e510250c6cca5b477d6ac46e8f39c4596fed2f77
BandhaviC <bandhavic6@gmail.com>
2024-01-08 17:08:09 -0500

    updated context

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,8 +9,9 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Bandhavi</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with over 3 years of experience using React, React Native and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a full-stack developer with over 3 years of experience using React, React Native and NodeJS. 
+          I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.
+          Reach out if you'd like to learn more!
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
